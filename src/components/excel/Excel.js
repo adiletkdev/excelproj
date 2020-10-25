8786329e9417642309cd2d948fc3314ff30de76c
Adilet K <adiletk312@gmail.com>
2020-10-25 18:28:43 +0600

    Test WebStorm UI for Git

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -12,7 +12,7 @@ export class Excel {
     this.components = this.components.map(Component => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      // DEBUG
+      // Debug
       // if (component.name) {
       //   window['c' + component.name] = component
       // }
