2931a654368270a203ddfd6b27d3c6b2be634b3e
Adilet K <adiletk312@gmail.com>
2020-12-20 18:44:42 +0600

    routing implemented

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,7 @@ import {$} from '@core/dom'
 import {changeTitle} from '@/redux/actions'
 import {defaultTitle} from '@/constants'
 import {debounce} from '@core/utils'
+import {ActiveRoute} from '@core/routes/ActiveRoute'
 
 export class Header extends ExcelComponent {
   static className = 'excel__header'
@@ -10,7 +11,7 @@ export class Header extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Header',
-      listeners: ['input'],
+      listeners: ['input', 'click'],
       ...options
     });
   }
@@ -27,11 +28,11 @@ export class Header extends ExcelComponent {
       <div>
 
         <div class="button">
-          <span class="material-icons">delete</span>
+          <span class="material-icons" data-button="remove">delete</span>
         </div>
 
         <div class="button">
-          <span class="material-icons">exit_to_app</span>
+          <span class="material-icons" data-button="exit">exit_to_app</span>
         </div>
 
       </div>
@@ -41,5 +42,19 @@ export class Header extends ExcelComponent {
   onInput(event) {
     const $target = $(event.target)
     this.$dispatch(changeTitle($target.text()))
+  }
+
+  onClick(event) {
+    const $target = $(event.target)
+    if ($target.data.button === 'remove') {
+      const decision = confirm('Вы действительно хотите удалить эту таблицу?')
+
+      if (decision) {
+        localStorage.removeItem('excel:' + ActiveRoute.param)
+        ActiveRoute.navigate('')
+      }
+    } else if ($target.data.button === 'exit') {
+      ActiveRoute.navigate('')
+    }
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const path = require('path')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
@@ -68,6 +69,9 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: filename('css')
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ],
   module: {
